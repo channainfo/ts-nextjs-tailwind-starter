4424a26211859101327c307164352bde66d6976c
Channa Ly <channa.info@gmail.com>
2023-04-02 21:46:46 +0700

    chore: connect to sui wallet using Ethos connect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
+import { EthosConnectProvider } from 'ethos-connect';
 import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
 import '@/styles/colors.css';
+
+// import { NETWORK } from '@/lib/constants';
 
 /**
  * !STARTERCONF info
@@ -10,7 +13,25 @@ import '@/styles/colors.css';
  */
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // return <Component {...pageProps} />;
+
+  // const ethosConfiguration = {
+  //   // When testing, use our staging link. When in production you may comment this line out.
+  //   // walletAppUrl: 'https://sui-wallet-staging.onrender.com',
+  //   apiKey: process.env.NEXT_PUBLIC_ETHOS_API_KEY,
+  //   preferredWallets: ['Ethos Wallet'],
+  //   network: NETWORK,
+  // };
+
+  return (
+    <EthosConnectProvider
+      // ethosConfiguration={ethosConfiguration}
+      dappName='EthosConnect Example App'
+      connectMessage='Your connect message goes here!'
+    >
+      <Component {...pageProps} />
+    </EthosConnectProvider>
+  );
 }
 
 export default MyApp;
